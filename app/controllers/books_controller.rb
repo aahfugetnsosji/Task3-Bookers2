7cfe3c04baf8b99e2442f8995bdf09df7c6dc072
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-02 14:53:06 +0000

    [Add] controller作成・ modelファイルへ1:Nの関係を記述・viewファイルへレイアウトを整えるための記述　追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -12,11 +14,20 @@ class BooksController < ApplicationController
   end
   
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(books.id)
   end
   
   def update
   end
   
   def destroy
+  end
+  
+  private
+  def book_params
+    params.repuire(:book).permit(:title, :body)
   end
 end
