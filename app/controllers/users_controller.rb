c71207ad67810926a01925db2e0908c7b03e06a5
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-06 15:40:22 +0000

    [Add] ユーザープロフィール編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   def index
     @users = User.all
     @book = Book.new
     @user = User.find(current_user.id)
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
   
   private
