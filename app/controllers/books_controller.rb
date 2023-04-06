48a590e24ac80f547aca66f0814da68aaaad52a0
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-06 14:45:33 +0000

    [Add] 投稿編集機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,12 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = User.find(@book.user_id)
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
   
   def create
@@ -23,9 +26,15 @@ class BooksController < ApplicationController
   end
   
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
   
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
