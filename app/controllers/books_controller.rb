b3c466b5ad2f042f06f34bdd72e6eaf88d086a8e
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-07 16:46:50 +0000

    [Add] ユーザープロフィール編集・新規投稿・投稿編集のバリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,18 +20,26 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user = User.find(current_user.id)
+      @books = Book.all
+      render :index
+    end
   end
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
