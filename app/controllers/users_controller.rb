610bf80ff0e03c30f834862e946c671cf1e12464
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-08 16:20:37 +0000

    [Add] アクセス制限：他人のユーザー編集画面へのアクセスを制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @book = Book.new
   end
 
   def edit
-    is_matching_login_user
     @user = User.find(params[:id])
   end
   
@@ -16,7 +17,6 @@ class UsersController < ApplicationController
   end
   
   def update
-    is_matching_login_user
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
