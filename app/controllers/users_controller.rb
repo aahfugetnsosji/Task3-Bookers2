ca83367f915fa14fc068c49eddf8d4ff5747504e
aahfugetnsosji <126599436+aahfugetnsosji@users.noreply.github.com>
2023-04-08 16:00:32 +0000

    [Update] 「New Book」のBを小文字に修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    is_matching_login_user
     @user = User.find(params[:id])
   end
   
@@ -15,6 +16,7 @@ class UsersController < ApplicationController
   end
   
   def update
+    is_matching_login_user
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
@@ -28,5 +30,12 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 end
